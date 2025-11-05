f1d5d811d6e017ab04d6d703844a7c27b3af0ba2
Nithya0501 <Nithyaselvakani96@gmail.com>
2025-11-05 06:34:19 -0800

    topic controller routes update

--- routes/topicRoutes.js
+++ routes/topicRoutes.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const {
   createTopic,
   createMultipleTopics,
-  getTopicsBySubject
+  getTopicsBySubject,
+  getTopic
 } = require("../controllers/topicController");
 
 router.post("/bulk", createMultipleTopics);
-router.post("/", createTopic);    
+router.post("/", createTopic);
 router.get("/:subjectId", getTopicsBySubject);
+router.get("/topic/:topicId", getTopic);
 
 module.exports = router;
 
